34dd6a5c17ac1327df3b170e906f3568718cb0d4
Anthony <Anthony@LAPTOP-U17VOOJC>
2021-09-29 12:52:38 +0200

    Dépot

---
+++ src/exo1/Exo1.java
@@ -0,0 +1,27 @@
+// NAVARRO Anthony TDB, 27/09/2021
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Bonjour");
+       String prenom;
+       Scanner sc;
+       sc = new Scanner(System.in);
+       System.out.println("Bonjour, Pouvez-vous me donner votre prenom ?");
+       prenom = sc.nextLine();
+       
+ 
+       
+    }
+    
+}
